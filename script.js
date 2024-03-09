6ae356bda63136174afd56f833cbdd81523fe61c
npx <tejasrigarapatilyros@gmail.com>
2024-03-09 09:32:29 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here, you can add code to handle form submission, such as sending an email
+    alert('Thank you for your message!');
+});
